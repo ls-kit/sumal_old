dcd055e16e00da527eff9049149dc972cca2d15e
dev-shakib <sakibkhan207@gmail.com>
2022-10-08 21:06:37 +0600

    show products in view for test

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,11 +1,45 @@
-@dd($products);
+
 
 @extends('shopify-app::layouts.default')
+@section('styles')
+<style>
+    ul {
+	display: flex;
+	flex-flow: row wrap;
+}
+    ul li {
+	flex-basis: 25%;
+	list-style: none;
+}
+</style>
+
+@endsection
 
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    <ul>
+        <li>Image </li>
+        <li>Title</li>
+        <li>Ceated At</li>
+        <li>Updated At</li>
+    </ul>
+    @foreach($products['products'] as $key => $product)
+        <ul>
+            <li><img src="{{$product->image['src'] }}" height="80px" width="80px" /></li>
+            <li>{{$product->title }}</li>
+            <li>{{$product->created_at }}</li>
+            <li>{{$product->updated_at }}</li>
+        </ul>
+    @endforeach
+
+
+
 @endsection
+
+
+
+
 
 @section('scripts')
     @parent
